795983c5ee30d95c243d5dee590e06225206a884
Slava Maksimov <hello@slavamak.com>
2022-04-09 00:50:26 +0300

    feat: add sanity redirects

--- web/next.config.js
+++ web/next.config.js
@@ -1,3 +1,28 @@
+const { createClient } = require("next-sanity")
+
+const sanityConfig = {
+  dataset: process.env.SANITY_PROJECT_DATASET || "production",
+  projectId: process.env.SANITY_PROJECT_ID,
+  useCdn: process.env.NODE_ENV === "production",
+  apiVersion: "2022-04-08",
+}
+
+const sanityClient = createClient(sanityConfig)
+
+async function fetchSanityRedirects() {
+  const data = await sanityClient.fetch(
+    `*[_type == "redirect"]{ from, to, isPermanent }`
+  )
+
+  const redirects = data.map((redirect) => ({
+    source: `${redirect.from}`,
+    destination: `${redirect.to}`,
+    permanent: redirect.isPermanent,
+  }))
+
+  return redirects
+}
+
 module.exports = {
   env: {
     SANITY_PROJECT_DATASET: process.env.SANITY_PROJECT_DATASET,
@@ -10,5 +35,9 @@ module.exports = {
   },
   images: {
     domains: ["cdn.sanity.io"],
+  },
+  async redirects() {
+    const sanityRedirects = await fetchSanityRedirects()
+    return sanityRedirects
   },
 }
