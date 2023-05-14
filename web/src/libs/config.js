d70efc1d7215cc1690d2e10912e25a7fbd5e00b7
Slava Maksimov <hello@slavamak.com>
2023-05-14 16:40:51 +0300

    chore: initial commit to start from scratch

--- web/src/libs/config.js
+++
@@ -1,8 +0,0 @@
-const sanityConfig = {
-  dataset: process.env.SANITY_PROJECT_DATASET || "production",
-  projectId: process.env.SANITY_PROJECT_ID,
-  useCdn: process.env.NODE_ENV === "production",
-  apiVersion: "2022-04-08",
-}
-
-export default sanityConfig
