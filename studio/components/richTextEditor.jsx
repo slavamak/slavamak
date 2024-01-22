563bbc804d08cda43ebe5198e99b4585883a6de3
Slava Maksimov <hello@slavamak.com>
2024-01-22 21:08:42 +0300

    chore: initial commit

--- studio/components/richTextEditor.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-
-// eslint-disable-next-line import/prefer-default-export
-export const SubTitleStyle = ({ children }) => (
-  <span style={{ fontSize: "1.5em" }}>{children}</span>
-)
--- studio/structure/deskStructure.js
+++
@@ -1,9 +0,0 @@
-import S from "@sanity/desk-tool/structure-builder"
-
-import Home from "./paneItems/pageHome"
-import Projects from "./paneItems/projects"
-import Redirects from "./paneItems/redirects"
-import Settings from "./paneItems/settings"
-
-export default () =>
-  S.list().title("Content").items([Home, Projects, Settings, Redirects])
