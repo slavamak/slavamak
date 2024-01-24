6b1a38e74b3e4818031257387ed69bd4a7db0cfb
Slava Maksimov <hello@slavamak.com>
2024-01-24 21:46:40 +0300

    feat: add layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://slavamak.com',
   integrations: [tailwind()]
 });
---
+++ src/consts.js
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Slava Maksimov | Independent Developer';
+export const SITE_DESCRIPTION = 'Independent developer crafting tailored e-commerce solutions and engaging web experiences with a commitment to performance and user-centric design.';
